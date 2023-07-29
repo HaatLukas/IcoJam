3fe3f86f24fb65d6248c7ac4d3ecd48f5257d0d9
claitz <stefanomus@fastmail.com>
2023-07-29 10:45:46 +0100

    Debug console CVAR `d.PrintDebug` - set to 1 to show debug

--- Source/IcoJam/Public/Debug.h
+++ Source/IcoJam/Public/Debug.h
@@ -4,8 +4,19 @@
 #include "Framework/Notifications/NotificationManager.h"
 #include "Widgets/Notifications/SNotificationList.h"
 
+static TAutoConsoleVariable CVShowDebug(
+	TEXT("d.PrintDebug"),
+	0,
+	TEXT("Draws debug info.\n")
+	TEXT("0: Disabled\n")
+	TEXT("1: Enabled"),
+	ECVF_Cheat);
+
+
 inline void Print(const UObject* Instigator,const FString &Message, const FColor &Color = FColor::Green, const float &Duration = 5.f)
 {
+	if (!CVShowDebug.GetValueOnAnyThread()) return;
+	
 	if (GEngine)
 	{
 		const FString InstigatorName = Instigator ? GetNameSafe(Instigator) : "None";
@@ -18,6 +29,13 @@ inline void Print(const UObject* Instigator,const FString &Message, const FColor
 inline void PrintLog(const FString &Message)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+}
+
+inline void DebugLine(const UWorld* World, const FVector& Start, const FVector& End, const FColor Color = FColor::Red, const float Duration = 2.f)
+{
+	if (!CVShowDebug.GetValueOnAnyThread()) return;
+	
+	DrawDebugLine(World, Start, End, Color, false, Duration, 0, 5.f);
 }
 
 inline EAppReturnType::Type ShowMsgDialog(const EAppMsgType::Type &MsgType, const FString &Message, const bool bError = false)
